c9e1d9b54b9a3acad99b29d3f988b6efe708cc74
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-08-06 18:02:32 +0530

    Update queue.py

--- BADMUSIC/utils/inline/queue.py
+++ BADMUSIC/utils/inline/queue.py
@@ -66,8 +66,55 @@ def queue_back_markup(_, CPLAY):
 def aq_markup(_, chat_id):
     buttons = [
         [
+            InlineKeyboardButton(text="II ", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="▢ ", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="‣‣ ", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▷ ", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="↺ ", callback_data=f"ADMIN Replay|{chat_id}"),
+        ],
+        [
             InlineKeyboardButton(text="𐏓 ⃪⃝🇺🇸 ꯭𝗗ᴇᴠɪ𝗟 ꯭༎ࠫ⛧‌", url="https://t.me/ll_mxni_ll"),
-            InlineKeyboardButton(text="⎯꯭̽🇨🇦꯭꯭ ⃪В꯭α꯭∂⎯꯭ ꯭̽🌸", url="https://t.me/II_BAD_BBY_II"),
+            InlineKeyboardButton(text="⎯꯭̽🇨🇦꯭꯭ ⃪В꯭α꯭∂⎯꯭ ꯭̽🌸", url="https://t.me/ll_BAD_MUNDA_ll"),
+        ],
+        [
+            InlineKeyboardButton(
+                text="๏ ᴍᴏʀᴇ ๏",
+                url="https://t.me/PBX_PERMOT",
+            ),
         ],
     ]
+    return buttons
+
+
+def queuemarkup(_, vidid, chat_id):
+
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_5"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="II ",
+                callback_data=f"ADMIN Pause|{chat_id}",
+            ),
+            InlineKeyboardButton(text="▢ ", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="‣‣ ", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▷ ", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="↺ ", callback_data=f"ADMIN Replay|{chat_id}"),
+        ],
+        [
+            InlineKeyboardButton(text="𐏓 ⃪⃝🇺🇸 ꯭𝗗ᴇᴠɪ𝗟 ꯭༎ࠫ⛧‌", url="https://t.me/ll_mxni_ll"),
+            InlineKeyboardButton(text="⎯꯭̽🇨🇦꯭꯭ ⃪В꯭α꯭∂⎯꯭ ꯭̽🌸", url="https://t.me/ll_BAD_MUNDA_ll"),
+        ],
+        [
+            InlineKeyboardButton(
+                text="๏ ᴍᴏʀᴇ ๏",
+                url="https://t.me/PBX_PERMOT",
+            ),
+        ],
+    ]
+
     return buttons
