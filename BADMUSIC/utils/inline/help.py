0798f1735bb0c1a4d457811ce87321daeb983105
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-08-06 19:51:12 +0530

    Update help.py

--- BADMUSIC/utils/inline/help.py
+++ BADMUSIC/utils/inline/help.py
@@ -78,12 +78,6 @@ def help_pannel(_, START: Union[bool, int] = None):
                     callback_data="help_callback hb9",
                 ),
             ],
-            [
-                InlineKeyboardButton(
-                    text="sᴛᴀʀᴛ",
-                    callback_data="help_callback hb11",
-                ),
-            ],
             mark,
         ]
     )
