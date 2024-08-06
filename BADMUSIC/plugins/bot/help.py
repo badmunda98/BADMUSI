7555842fbfb1feafe4cedcb015481c8e18594212
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-08-06 18:14:26 +0530

    Update help.py

--- BADMUSIC/plugins/bot/help.py
+++ BADMUSIC/plugins/bot/help.py
@@ -108,20 +108,7 @@ async def helper_cb(client, CallbackQuery, _):
         await CallbackQuery.edit_message_text(
             helpers.HELP_10, reply_markup=keyboard
         )
-    elif cb == "hb11":
-        await CallbackQuery.edit_message_text(
-            helpers.HELP_11, reply_markup=keyboard
-        )
     elif cb == "hb12":
         await CallbackQuery.edit_message_text(
             helpers.HELP_12, reply_markup=keyboard
         )
-    elif cb == "hb13":
-        await CallbackQuery.edit_message_text(
-            helpers.HELP_13, reply_markup=keyboard
-        )
-    elif cb == "hb14":
-        await CallbackQuery.edit_message_text(
-            helpers.HELP_14, reply_markup=keyboard
-  )
-          
