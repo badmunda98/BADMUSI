94b7df16070bcbeb60131d7cc8d09a75659eea75
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-08-06 19:38:12 +0530

    Update help.py

--- BADMUSIC/utils/inline/help.py
+++ BADMUSIC/utils/inline/help.py
@@ -15,7 +15,10 @@ def help_pannel(_, START: Union[bool, int] = None):
         InlineKeyboardButton(
             text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
         ),
-            InlineKeyboardButton(text=" ʜᴇʟᴘ ", callback_data="settings_back_helper"),
+            InlineKeyboardButton(
+            text=_["BACK_BUTTON"],
+            callback_data=f"settingsback_helper",
+        ),
     ]
     mark = second if START else first
     upl = InlineKeyboardMarkup(
